a7b87c33a53f0a2edf8190775729fe3cbc0fccea
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-03-24 00:05:56 -0300

    migrated from tailwind css to chakra ui, preparing application to support new front end features

--- src/app/helper/buttons.ts
+++
@@ -1,9 +0,0 @@
-import {
-    binaryOperators,
-    unaryBinaryOperators,
-    unaryOperators,
-} from "./pratt-parser/lexer";
-
-export const buttons = binaryOperators
-    .concat(unaryBinaryOperators)
-    .concat(unaryOperators);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
 "use client";
 
-import { InputContextProvider } from "./contexts/InputContext";
-import { Buttons } from "./components/Buttons";
-import { InputSpace } from "./components/InputSpace";
-import { StartButton } from "./components/StartButton";
-import { OutputSpace } from "./components/OutputSpace";
-
 export default function Home() {
-    return (
-        <InputContextProvider>
-            <div className="flex justify-center">
-                <div className="flex flex-col justify-center px-12 space-y-6 max-w-[720px] w-[100%] h-screen">
-                    <div className="flex items-center space-x-4 py-4">
-                        <InputSpace />
-                        <StartButton />
-                    </div>
-
-                    <OutputSpace />
-
-                    <Buttons />
-                </div>
-            </div>
-        </InputContextProvider>
-    );
+	return <></>;
 }
